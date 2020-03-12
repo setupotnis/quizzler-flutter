c251f56867466e87ebd389c1f5f88f83342d3deb
Setu Potnis <setupotnis@gmail.com>
2020-03-12 11:52:37 -0400

    created questions class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'questions.dart';
 
 void main() => runApp(Quizzler());
 
@@ -26,12 +27,14 @@ class QuizPage extends StatefulWidget {
 
 class _QuizPageState extends State<QuizPage> {
   List<Widget> scoreKeeper = [];
-  List<String> questions = [
-    'You can lead a cow down stairs but not up stairs.',
-    'Approximately one quarter of human bones are in the feet.',
-    'A slug\'s blood is green.'
+  List<Question> questionBank = [
+  Question(q: 'You can lead a cow down stairs but not up stairs.', a: false);
+  Question(q: 'Approximately one quarter of human bones are in the feet.', a: true);
+  Question(q: 'A slug\'s blood is green.', a: true);
   ];
+
   int questionNumber = 0;
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -69,8 +72,15 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 // The user picked true
-                setState(() {});
-                questionNumber++;
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == true) {
+                  print('User got it right');
+                } else {
+                  print('User got it wrong');
+                }
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
@@ -89,6 +99,9 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  questionNumber++;
+                });
               },
             ),
           ),
